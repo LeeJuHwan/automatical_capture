7ac723c74ab5c87f0215367c7bbcc8a59dcf83d7
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2023-06-02 13:06:00 +0900

    Fix: Images to PDF sort

--- setup/setup_files.py
+++ setup/setup_files.py
@@ -3,6 +3,7 @@ from PIL import Image
 import os
 import shutil
 from setup.setup_directory import create_screenshot_directory
+import re
 
 
 def change_screenshot_file_name(flag):
@@ -15,19 +16,37 @@ def change_screenshot_file_name(flag):
     subprocess.run(["/bin/bash", "-c", cmd])
 
 
+def extract_number(filename):
+    """파일 이름에서 숫자를 추출하는 함수"""
+    pattern = r"\d+"
+    match = re.search(pattern, filename)
+    if match:
+        return int(match.group())
+    return 0
+
+
+def sort_screenshots(lst):
+    """파일 이름에서 숫자를 추출하여 정렬하는 함수"""
+    sorted_lst = sorted(lst, key=extract_number)
+    return sorted_lst
+
+
 def convert_rgb_images(screenshot_folder):
     """이미지 파일을 RGB값으로 변경 합니다."""
+    sorted_screenshot_folder = sort_screenshots(os.listdir(screenshot_folder))
+
     images = [
         Image.open(os.path.join(screenshot_folder, files)).convert("RGB")
-        for files in os.listdir(screenshot_folder)
+        for files in sorted_screenshot_folder
     ]
+
     return images
 
 
 def create_pdf_to_images(pdf_folder, images):
     """스크린샷 파일들을 하나의 PDF로 생성 합니다."""
     pdf_file_name = os.path.join(pdf_folder, "capture.pdf")
-    images[0].save(pdf_file_name, save_all=True, append_images=images)
+    images[0].save(pdf_file_name, save_all=True, append_images=images[1:])
 
 
 def remove_screenshot_files(screenshot_folder):
