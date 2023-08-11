4d65a594015f17470ca193a12a7f4e1117b1af7d
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2023-08-12 01:57:06 +0900

    Feat: capture shell script for easy of use

---
+++ capture.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+PYTHON_FILE=save_capture.py
+
+
+while :
+do
+	echo "============= capture ============="
+	printf "1: %-10s\n" "start capture program"
+	echo "q: to quit"
+	echo "==============================="
+	echo "choose index number: "
+	printf "> "
+	read INDEX
+
+	if [ "$INDEX" = "1" ]; then
+		while :
+		do
+			echo "========== QR Pay Scripts =========="
+	       		printf "1: %-10s\n" "CAPTURE PAGE"
+        		printf "2: %-10s\n" "MODIFY PARAMETER"
+		        echo "q: TO QUIT"
+       			echo "==============================="
+	       		echo "choose index number: "
+	       		printf "> "
+			read INDEX2
+			
+    			if [ "$INDEX2" = "1" ]; then
+				echo "page: "
+				printf "> "
+				read PAGE
+				python3 $PYTHON_FILE -p $PAGE
+
+			elif [ "$INDEX2" = "2" ]; then
+				echo "example -> -p {page} -c {0} -r {0}"
+				echo "parameter: "
+				printf "> " 
+				read PARAMS
+				python3 $PYTHON_FILE $PARAMS
+       			else
+               		 	break
+        		fi
+		done
+		
+	elif [ "$INDEX" = "q" ]; then
+        	exit
+	else
+        	echo "Invalid input. Please enter a valid index or 'q' to quit."
+		continue
+	fi
+done	
+
